f0fdba9c2a0acdf2d977ff1a097406c94d721553
/Nexus_Prime <nicolas.estevao.lima1004@hotmail.com>
2023-11-07 13:18:44 -0300

    margin left to price on product page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import Actions from "./actions";
 import Image from "next/image";
 import prisma from '@lib/database';
+import format from "@lib/formatters";
 
 export default async function Product({ params }: { params: { id: string } }) {
 	const product = await prisma.product.findUnique({
@@ -11,11 +12,6 @@ export default async function Product({ params }: { params: { id: string } }) {
 	if (!product) {
 		return notFound();
 	}
-
-	const formatter = new Intl.NumberFormat("pt-BR", {
-		style: "currency",
-		currency: "BRL",
-	});
 
 	return (
 		<>
@@ -29,7 +25,7 @@ export default async function Product({ params }: { params: { id: string } }) {
 
 			<div className="mx-8 mt-5 flex items-center justify-between">
 				<h1 className="font-semibold">{product.name}</h1>
-				<h2>{formatter.format(product.price)}</h2>
+				<h2 className="ml-2">{format.brl(product.price)}</h2>
 			</div>
 			<p className="mx-7 my-5 min-h-[5rem] max-h-40 font-light break-words overflow-hidden">{product.description}</p>
 
