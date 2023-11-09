bf3ad4ed2dd8f27307b82e8a09ac799ba856705a
/Nexus_Prime <nicolas.estevao.lima1004@hotmail.com>
2023-11-09 02:17:31 -0300

    help btn to inputs added

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -2,16 +2,19 @@
 
 import React from 'react';
 import { FieldError } from 'react-hook-form';
+import { Popover, PopoverContent, PopoverTrigger } from './Popover';
+import TouchableIcon from '@components/TouchableIcon';
 
 interface Props extends React.HTMLProps<HTMLInputElement> {
 	format?: (...args: any) => any;
 	label?: string;
 	error?: FieldError;
+	info?: { title: string, description: string };
 }
 type Ref = HTMLInputElement;
 
 const Input = React.forwardRef<Ref, Props>(({ format, onChange, value, ...props }, ref) => {
-	const [content, setContent] = React.useState(props.defaultValue);
+	const [content, setContent] = React.useState(props.defaultValue || '');
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setContent(format
@@ -24,7 +27,11 @@ const Input = React.forwardRef<Ref, Props>(({ format, onChange, value, ...props 
 
 	return (
 		<div>
-			{!!props.label && <label>{props.label}</label>}
+			<div className='flex flex-row items-center justify-between'>
+				{!!props.label && <label>{props.label}</label>}
+				
+				{!!props.info && <HelpPopover content={props.info} /> }
+			</div>
 			<input
 				ref={ref}
 				value={content}
@@ -37,6 +44,35 @@ const Input = React.forwardRef<Ref, Props>(({ format, onChange, value, ...props 
 		</div>
 	)
 })
+
+function HelpPopover({ content }) {
+	const [open, setOpen] = React.useState(false);
+
+
+	React.useEffect(() => {
+		if(!open) return;
+
+		const timeout = setTimeout(() => setOpen(false), 5000);
+
+		return () => {
+			clearTimeout(timeout);
+		}
+	}, [open]);
+	
+	return (
+		<Popover onOpenChange={setOpen} open={open}>
+			<PopoverTrigger asChild>
+				<button>
+					<TouchableIcon type='HelpCircle' size={20} className='mx-3'/>
+				</button>
+			</PopoverTrigger>
+			<PopoverContent className='bg-[rgb(var(--foreground-rgb))] border-[rgb(var(--detail-rgb))] mr-5 w-40'>
+				<h5>{content.title}</h5>
+				<p className='text-xs'>{content.description}</p>
+			</PopoverContent>
+		</Popover>
+	);
+}
 
 Input.displayName = 'Input'
 
