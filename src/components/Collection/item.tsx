eba6045126cc1b95d4b474864dfddc5cba12d509
/Nexus_Prime <nicolas.estevao.lima1004@hotmail.com>
2023-11-27 21:54:52 -0300

    Collection Component reajusted to lg screens

--- src/components/Collection/item.tsx
+++ src/components/Collection/item.tsx
@@ -30,11 +30,11 @@ export const Item: React.FC<{ data: Product }> = ({ data }) => {
 
 export function SkeletonItem() {
 	return (
-		<div className="w-28 mr-4 relative flex shrink-0 flex-col">
+		<div className="w-28 md:w-[15vw] lg:w-[12vw] mr-4 relative flex shrink-0 flex-col">
 			<div className="w-full relative">
-				<Skeleton className="rounded-lg aspect-square" />
+				<Skeleton className="rounded-lg aspect-square md:rounded-b-none" />
 			</div>
-			<Skeleton className="mt-2 h-5 w-full" />
+			<Skeleton className="mt-2 h-5 md:h-[4.6rem] md:m-0 w-full md:rounded-t-none" />
 		</div>
 	)
 }
